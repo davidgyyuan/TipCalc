b53d1dd2d76ee3f19e69023decd64468348cc370
David Yuan <Yuan-MBPr@Davids-MacBook-Pro-2.local>
2017-01-06 00:25:21 -0600

    Here is my commit message

---
+++ TipCalc/SettingsViewController.swift
@@ -0,0 +1,48 @@
+//
+//  SettingsViewController.swift
+//  TipCalc
+//
+//  Created by David Yuan on 1/5/17.
+//  Copyright © 2017 David Yuan. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBOutlet weak var field: UITextField!
+    @IBAction func EditChange(_ sender: Any) {
+        let defaults = UserDefaults.standard
+        defaults.set(Double(field.text!) ?? 0, forKey: "customPercent")
+        defaults.synchronize()
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let defaults = UserDefaults.standard
+        if defaults.double(forKey: "customPercent") > 0 {
+            field.text = String(defaults.double(forKey: "customPercent"))
+        }
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- TipCalc/ViewController.swift
+++ TipCalc/ViewController.swift
@@ -10,16 +10,63 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    @IBOutlet weak var splitSlider: UISlider!
+    @IBOutlet weak var splitLabel: UILabel!
+    var cusPer = Double(25)
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
-
+    @IBAction func changeSplit(_ sender: Any) {
+        splitLabel.text=String(Int(splitSlider.value))
+        update()
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        billField.becomeFirstResponder()
+        let defaults = UserDefaults.standard
+        
+       // print("a")
+        cusPer = defaults.double(forKey: "customPercent")
+        if cusPer <= 0{
+            cusPer = 25
+        }
+        tipControl.setTitle("\(cusPer)%", forSegmentAt: 3)
+        update()
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTap(_ sender: Any) {
+        //view.endEditing(true)
+    }
+    @IBAction func calcTip(_ sender: Any) {
+        update()
+    }
+    @IBAction func valChanged(_ sender: Any) {
+        update()
+    }
+    func update(){
+        let tipPercentages = [0.15, 0.18, 0.22,cusPer/100]
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total/Double(splitLabel.text!)!)
+
+    }
+    
 
 }
 
